9c62566da81030057fdd29ac501f041dc3e7de5f
GregoryVDev <arexo@hotmail.fr>
2025-03-07 19:10:23 +0100

    Edited the burger icon and added the property for header and burger menu. Removed the script js

--- src/template/navbar.php
+++ src/template/navbar.php
@@ -25,18 +25,26 @@
             </div>
         </div>
         <div id="burger-menu">
-            <svg width="40" height="35" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M5 12H19M3 7H21M8 17H16" stroke="var(--colorOrange)" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
+            <svg width="40" height="35" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <g stroke="black" stroke-width="1.5" stroke-linecap="round" stroke="var(--colorBlue)">
+                    <line x1="5" y1="7" x2="19" y2="7" />
+                    <line x1="7" y1="12" x2="17" y2="12" />
+                    <line x1="5" y1="17" x2="19" y2="17" />
+                </g>
             </svg>
-        </div>
-        <nav id="nav">
-            <ul class="nav_list">
-                <li><a href="../index.php">Accueil</a></li>
-                <li><a href="#">A propos</a></li>
-                <li><a href="#">Services</a></li>
-                <li><a href="#">Formules</a></li>
-                <li><a href="#">Avantages</a></li>
-                <li><a href="#">FAQ</a></li>
-            </ul>
-        </nav>
+
+            <nav id="nav">
+                <ul class="nav_list">
+                    <li><a href="../index.php">Accueil</a></li>
+                    <li><a href="#">A propos</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Formules</a></li>
+                    <li><a href="#">Avantages</a></li>
+                    <li><a href="#">FAQ</a></li>
+                </ul>
+            </nav>
     </header>
+</body>
+<script src="../js/burger.js"></script>
+
+</html>
