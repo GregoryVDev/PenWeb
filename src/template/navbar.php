42e16b49f2fc1c0a5fe110b7b5d6eeb8963e5eee
GregoryVDev <arexo@hotmail.fr>
2025-03-07 18:28:24 +0100

    First Commit

---
+++ src/template/navbar.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/login.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/fonts.css">
+    <link rel="stylesheet" href="../css/index.css">
+    <link rel="stylesheet" href="../template/navbar.css">
+    <link rel="stylesheet" href="../template/footer.css">
+    <link rel="stylesheet" href="../css/index.css">
+    <title>PulseFit</title>
+</head>
+
+<body>
+    <header>
+        <div class=" container-left">
+            <div class="logo">
+                <h1>PulseFit</h1>
+            </div>
+        </div>
+        <div id="burger-menu">
+            <svg width="40" height="35" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M5 12H19M3 7H21M8 17H16" stroke="var(--colorOrange)" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
+            </svg>
+        </div>
+        <nav id="nav">
+            <ul class="nav_list">
+                <li><a href="../index.php">Accueil</a></li>
+                <li><a href="../coachs.php">Coachs</a></li>
+                <li><a href="../reviews.php">Services</a></li>
+                <li><a href="../connexion.php">Formules</a></li>
+                <li><a href="../inscription.php">Contact</a></li>
+            </ul>
+            <div class="container-buttons">
+                <img src="../images/logos/circle-user.svg" alt="">
+                <a href="../connexion.php" class="button-connection">Connexion</a>
+                <div class="container-reservation">
+                    <a href="../inscription.php" class="button-reservation">Réserver</a>
+                </div>
+            </div>
+        </nav>
+    </header>
