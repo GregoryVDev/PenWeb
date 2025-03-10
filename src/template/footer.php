29d068974ebf7c48c937754ab93a5a2f6a49c222
GregoryVDev <arexo@hotmail.fr>
2025-03-10 20:41:14 +0100

    Added image transparent for the footer

---
+++ src/template/footer.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/fonts.css">
+    <link rel="stylesheet" href="../css/index.css">
+    <link rel="stylesheet" href="../template/navbar.css">
+    <link rel="stylesheet" href="../template/footer.css">
+    <link rel="stylesheet" href="../css/index.css">
+    <title>PenWeb</title>
+</head>
+
+<body>
+    <footer>
+        <div class="container-logo">
+            <img src="../images/logos/penwebtrans.png" alt="Logo entreprise">
+        </div>
+        <div class="container-ul">
+            <h3>Menu</h3>
+            <ul>
+                <li><a href="#">Accueil</a></li>
+                <li><a href="#">Coachs</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Formules</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </div>
+        <div class="container-support">
+            <h3>Contactez-nous</h3>
+            <p>Adresse : 15 rue du château 75001 Paris, France</p>
+            <p>E-mail : info@pulsefit.fr</p>
+            <p>Tel : 01 23 45 67 89</p>
+        </div>
+        <div class="container-work">
+            <h3>Horaires</h3>
+            <p>Lun - Sam : 6h - 23h</p>
+            <p>Dimanche : 9h - 19h</p>
+            <p class="orange">Fermé les jours fériés</p>
+        </div>
+    </footer>
+    <div class="footer-bottom">
+        <div class="politiques">
+            <p><a href="#">Mentions légales</a></p>
+            <p><a href="#">Politique de confidentialité</a></p>
+            <p><a href="#">Condition d'utilisation</a></p>
+        </div>
+        <p><a href="#">© 2025 - Grégory</a></p>
+    </div>
+</body>
+<script src="../js/burger.js"></script>
+<script src="../js/carousel.js"></script>
+
+</html>
