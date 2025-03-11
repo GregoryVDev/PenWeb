369d6f7f7161a6dcf7c9a8d1358840eb4815fc4c
GregoryVDev <arexo@hotmail.fr>
2025-03-11 20:52:24 +0100

    Edited footer and property footer. Removed h3 in footer

--- src/template/footer.php
+++ src/template/footer.php
@@ -19,34 +19,24 @@
             <img src="../images/logos/penwebtrans.png" alt="Logo entreprise">
         </div>
         <div class="container-ul">
-            <h3>Menu</h3>
             <ul>
                 <li><a href="#">Accueil</a></li>
-                <li><a href="#">Coachs</a></li>
+                <li><a href="#">A propos</a></li>
                 <li><a href="#">Services</a></li>
-                <li><a href="#">Formules</a></li>
-                <li><a href="#">Contact</a></li>
+                <li><a href="#">FAQ</a></li>
             </ul>
         </div>
         <div class="container-support">
-            <h3>Contactez-nous</h3>
-            <p>Adresse : 15 rue du château 75001 Paris, France</p>
-            <p>E-mail : info@pulsefit.fr</p>
-            <p>Tel : 01 23 45 67 89</p>
+            <p>contact@gmail.com</p>
+            <p>01 23 45 67 89</p>
         </div>
         <div class="container-work">
-            <h3>Horaires</h3>
-            <p>Lun - Sam : 6h - 23h</p>
-            <p>Dimanche : 9h - 19h</p>
-            <p class="orange">Fermé les jours fériés</p>
-        </div>
-    </footer>
-    <div class="footer-bottom">
-        <div class="politiques">
             <p><a href="#">Mentions légales</a></p>
             <p><a href="#">Politique de confidentialité</a></p>
             <p><a href="#">Condition d'utilisation</a></p>
         </div>
+    </footer>
+    <div class="footer-bottom">
         <p><a href="#">© 2025 - Grégory</a></p>
     </div>
 </body>
