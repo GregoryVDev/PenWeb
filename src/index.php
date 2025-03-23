be9ebe71b7e29616bd165f0126f627003437a04d
GregoryVDev <arexo@hotmail.fr>
2025-03-23 19:51:39 +0100

    Added container services and added in queries tablet display and flex wrap

--- src/index.php
+++ src/index.php
@@ -22,69 +22,71 @@
     </section>
     <section class="services">
         <h2>Nos services pour la création de votre site internet</h2>
-        <article class="service">
-            <figure>
-                <img src="./images/logos/creation.png" alt="Création site web">
-                <figcaption>
-                    <h3>Création de site web</h3>
-                    <p>Nous réalisons des sites vitrines, sites e-commerce ou sur-mesure.</p>
-                </figcaption>
-            </figure>
-        </article>
-        <article class="service">
-            <figure>
-                <img src="./images/logos/responsive.png" alt="Responsive">
-                <figcaption>
-                    <h3>Site internet Responsive</h3>
-                    <p>Nos sites sont adaptés à tous types d'appareils pour une expérience optimale.</p>
-                </figcaption>
-            </figure>
-        </article>
-        <article class="service">
-            <figure>
-                <img src="./images/logos/seo.png" alt="Référencement SEO">
-                <figcaption>
-                    <h3>Référencement SEO</h3>
-                    <p>Chaque projet est optimisé pour le référencement SEO afin d'attirer plus de visiteurs.</p>
-                </figcaption>
-            </figure>
-        </article>
-        <article class="service">
-            <figure>
-                <img src="./images/logos/backend.png" alt="Outil d'automatisation">
-                <figcaption>
-                    <h3>Outil d'automatisation</h3>
-                    <p>Chaque projet est optimisé pour le référencement SEO afin d'attirer plus de visiteurs.</p>
-                </figcaption>
-            </figure>
-        </article>
-        <article class="service">
-            <figure>
-                <img src="./images/logos/chatbot.png" alt="Intelligence Artificielle">
-                <figcaption>
-                    <h3>Intelligence Artificielle</h3>
-                    <p>Nous réalisons des sites vitrines, sites e-commerce ou sur-mesure.</p>
-                </figcaption>
-            </figure>
-        </article>
-        <article class="service">
-            <figure>
-                <img src="./images/logos/hebergement.png" alt="Hébergement du site web">
-                <figcaption>
-                    <h3>Hébergement</h3>
-                    <p>Nous réalisons des sites vitrines, sites e-commerce ou sur-mesure.</p>
-                </figcaption>
-            </figure>
-        </article>
-        <article class="service">
-            <figure>
-                <img src="./images/logos/maintenance.png" alt="Maintenance">
-                <figcaption>
-                    <h3>Maintenance des sites et serveurs</h3>
-                    <p>Nous réalisons des sites vitrines, sites e-commerce ou sur-mesure.</p>
-                </figcaption>
-            </figure>
-        </article>
+        <div class="container-services">
+            <article class="service">
+                <figure>
+                    <img src="./images/logos/creation.png" alt="Création site web">
+                    <figcaption>
+                        <h3>Création de site web</h3>
+                        <p>Nous réalisons des sites vitrines, sites e-commerce ou sur-mesure.</p>
+                    </figcaption>
+                </figure>
+            </article>
+            <article class="service">
+                <figure>
+                    <img src="./images/logos/responsive.png" alt="Responsive">
+                    <figcaption>
+                        <h3>Site internet Responsive</h3>
+                        <p>Nos sites sont adaptés à tous types d'appareils pour une expérience optimale.</p>
+                    </figcaption>
+                </figure>
+            </article>
+            <article class="service">
+                <figure>
+                    <img src="./images/logos/seo.png" alt="Référencement SEO">
+                    <figcaption>
+                        <h3>Référencement SEO</h3>
+                        <p>Chaque projet est optimisé pour le référencement SEO afin d'attirer plus de visiteurs.</p>
+                    </figcaption>
+                </figure>
+            </article>
+            <article class="service">
+                <figure>
+                    <img src="./images/logos/backend.png" alt="Outil d'automatisation">
+                    <figcaption>
+                        <h3>Outil d'automatisation</h3>
+                        <p>Chaque projet est optimisé pour le référencement SEO afin d'attirer plus de visiteurs.</p>
+                    </figcaption>
+                </figure>
+            </article>
+            <article class="service">
+                <figure>
+                    <img src="./images/logos/chatbot.png" alt="Intelligence Artificielle">
+                    <figcaption>
+                        <h3>Intelligence Artificielle</h3>
+                        <p>Nous réalisons des sites vitrines, sites e-commerce ou sur-mesure.</p>
+                    </figcaption>
+                </figure>
+            </article>
+            <article class="service">
+                <figure>
+                    <img src="./images/logos/hebergement.png" alt="Hébergement du site web">
+                    <figcaption>
+                        <h3>Hébergement</h3>
+                        <p>Nous réalisons des sites vitrines, sites e-commerce ou sur-mesure.</p>
+                    </figcaption>
+                </figure>
+            </article>
+            <article class="service">
+                <figure>
+                    <img src="./images/logos/maintenance.png" alt="Maintenance">
+                    <figcaption>
+                        <h3>Maintenance des sites et serveurs</h3>
+                        <p>Nous réalisons des sites vitrines, sites e-commerce ou sur-mesure.</p>
+                    </figcaption>
+                </figure>
+            </article>
+        </div>
     </section>
     <section class="prices">
         <h2>Tarifs pour la création de votre site web personnalisé.</h2>
